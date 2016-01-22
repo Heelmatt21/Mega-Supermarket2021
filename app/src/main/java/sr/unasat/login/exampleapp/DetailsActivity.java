426dd374e0f9479cb021304a0eb646008c6f5b94
Maarten Narain <m.narain@unasat.sr>
2016-01-22 05:13:22 -0300

    init

---
+++ app/src/main/java/sr/unasat/login/exampleapp/DetailsActivity.java
@@ -0,0 +1,57 @@
+package sr.unasat.login.exampleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        loadWelcomeMessage();
+    }
+
+    private void loadWelcomeMessage() {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        TextView textView = (TextView) findViewById(R.id.welcomeMessage);
+        textView.setText(message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_details_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.logoutUser) {
+            logoutUser();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void logoutUser() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
