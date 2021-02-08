6d589a7875f2a5cf85fbcfe1069e583686a66405
Heelmatt21 <m.sahit@unasat.sr>
2021-02-07 21:49:14 -0300

    Foodordering App

---
+++ SMRFoodordering/app/src/main/java/sr/unasat/smrfoodordering/Cajunfries.java
@@ -0,0 +1,14 @@
+package sr.unasat.smrfoodordering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cajunfries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cajunfries);
+    }
+}
---
+++ SMRFoodordering/app/src/main/java/sr/unasat/smrfoodordering/MainActivity.java
@@ -0,0 +1,52 @@
+package sr.unasat.smrfoodordering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText Username;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Username = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Info.setText("Customer service: +5978587217");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Username.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+
+    }
+
+    private void validate(String userName, String userPassword) {
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+                Login.setEnabled(true);
+
+
+    }
+}
